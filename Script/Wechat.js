5fbe2d74f7f8926a71d00ff8a6c3f47e09465218
Eric <Alex0510@users.noreply.github.com>
2019-09-22 23:00:08 +0800

    Update Wechat.js

--- Script/Wechat.js
+++ Script/Wechat.js
@@ -1,6 +1,6 @@
 /*
 [Script]
-http-request ^https://mp\.weixin\.qq\.com/mp/getappmsgad script-path=https://Choler.github.io/Surge/Script/WeChat.js
+http-request ^https://mp\.weixin\.qq\.com/mp/getappmsgad script-path=https://raw.githubusercontent.com/Alex0510/surge/master/Script/Wechat.js
 
 [MITM]
 hostname = mp.weixin.qq.com
