d40ecee25c57635b895b68eb794fd31f4abbbf78
Eric <Alex0510@users.noreply.github.com>
2019-09-30 22:27:16 +0800

    Update shuiyinjingling.js

--- Script/shuiyinjingling.js
+++ Script/shuiyinjingling.js
@@ -1,6 +1,6 @@
 
 var obj = JSON.parse($response.body);
 
-obj = {"status":200,"msg":"\u83b7\u53d6\u6210\u529f","data":{"","is_vip":1,"vip_end_time":"2099-05-02"}}
+ {"status":200,"msg":"\u83b7\u53d6\u6210\u529f","data":{"","is_vip":1,"vip_end_time":"2099-05-02"}}
 
 $done({body: JSON.stringify(obj)});
