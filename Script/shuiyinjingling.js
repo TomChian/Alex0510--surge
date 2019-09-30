fef16790ca218886e598afbcf974e5ef21009e85
Eric <Alex0510@users.noreply.github.com>
2019-09-30 22:04:13 +0800

    Update shuiyinjingling.js

--- Script/shuiyinjingling.js
+++ Script/shuiyinjingling.js
@@ -2,6 +2,7 @@
 // http-response ^https://api1.dobenge.cn/api/user/ script-path=https://raw.githubusercontent.com/Alex0510/surge/master/Script/shuiyinjingling.js,requires-body=true
 // [MITM]
 // hostname = api1.dobenge.cn
+
 var obj = JSON.parse($response.body);
 
 obj = {"status":200,"msg":"\u83b7\u53d6\u6210\u529f","data":{"","is_vip":1,"vip_end_time":"2099-05-02"}}
