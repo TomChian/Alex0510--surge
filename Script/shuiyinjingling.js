af94615bed5248b5569025d5a02e118e12d65487
Eric <Alex0510@users.noreply.github.com>
2019-10-01 18:16:13 +0800

    Update shuiyinjingling.js

--- Script/shuiyinjingling.js
+++ Script/shuiyinjingling.js
@@ -1,6 +1,18 @@
 
 var obj = JSON.parse($response.body);
 
- var = {"status":200,"msg":"\u83b7\u53d6\u7528\u6237\u4fe1\u606f\u6210\u529f","is_vip":1,"vip_end_time":"2099-05-02"}
+ var = {
+  "status": 200,
+  "msg": "获取成功",
+  "data": {
+    "nick_name": "半根",
+    "gender": 0,
+    "mobile": "",
+    "is_vip": 1,
+    "vip_end_time": "2099-05-05",
+    "user_id": 20245,
+    "head_img": "https://thirdwx.qlogo.cn/mmopen/vi_32/Q3auHgzwzM5GawY2JqGFcqUicbibN4V5E2hr4NnA88Z4x2rIicaf4PQWHV0RAKO5HCLjy9Lr53yLaIa3PHhUUb0VQ/132"
+  }
+}
 
 $done({body: JSON.stringify(obj)});
