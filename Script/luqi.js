c2d848c6462d965c836f232177941097ce486582
Eric <Alex0510@users.noreply.github.com>
2019-10-23 14:42:36 +0800

    Update luqi.js

--- Script/luqi.js
+++ Script/luqi.js
@@ -4,11 +4,8 @@ obj.data={
     "login_num": 1,
     "nickname": "我是你爸爸",
     "is_vip": 1,
-    "vip_endtime": 1,
-    "expiretime": 1,
-    "_vip_endtime": "4081672705",
-    "expires_in": 4081672705,
-    "createtime": 1571804040,
+    "vip_endtime": 4081672705,
+ 
   }
 
 
