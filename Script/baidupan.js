6557435479def84bea6d1c4558cc0c81b8f8d85d
Eric <Alex0510@users.noreply.github.com>
2019-11-27 13:35:06 +0800

    Update baidupan.js

--- Script/baidupan.js
+++ Script/baidupan.js
@@ -1,10 +1,10 @@
 
-*/
+
 var url = $request.url;
 var murl = url.replace(/baidu/i, "baiduwp");
 var furl = murl.replace(/https/i, "alook");
 console.log(furl);
-var title = "Meeta 正在为您解析百度云盘分享链接";
+var title = "正在为您解析百度云盘分享链接";
 var subtitle = "如需下载该文件请下拉通知点击链接跳转";
 $notification.post(title, subtitle, furl);
 
