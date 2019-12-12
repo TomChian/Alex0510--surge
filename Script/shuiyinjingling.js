02b2ec9537ed192e4939d5b446c70c35c45a1103
Eric <Alex0510@users.noreply.github.com>
2019-12-12 16:51:52 +0800

    Update shuiyinjingling.js

--- Script/shuiyinjingling.js
+++ Script/shuiyinjingling.js
@@ -1,7 +1,6 @@
-let obj = JSON.parse($response.body);
+var obj = JSON.parse($response.body);
 
   obj.data["is_vip"］= 1,
   obj.data["vip_end_time"］= "2099-05-05"
-
 
 $done({body: JSON.stringify(obj)});
