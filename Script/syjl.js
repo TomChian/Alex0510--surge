8b8f30db1ad03275e010d5b8a8ebaff20e628964
Eric <Alex0510@users.noreply.github.com>
2019-12-14 10:31:53 +0800

    Update syjl.js

--- Script/syjl.js
+++ Script/syjl.js
@@ -2,7 +2,7 @@
 var obj = JSON.parse($response.body);
 
   obj. data= {
-    "user_id": 398201034,
+    "user_id": 叫爸爸,
     "nick_name": "我是你爸爸",
     "head_img":"https://img2.woyaogexing.com/2017/12/28/2faa32ef81d9d6dc!400x400_big.jpg",
     "is_vip": 1,
