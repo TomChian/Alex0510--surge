b8a1c85d83e6a4e53d6b14a911a557fe38b1bc71
Eric <Alex0510@users.noreply.github.com>
2020-02-17 17:43:56 +0800

    Update wps1.js

--- Script/wps1.js
+++ Script/wps1.js
@@ -1,8 +1,7 @@
 let obj = JSON.parse($response.body);
 
-obj = "vip": {
-    "memberid": 20,
-    "expire_time": 1739785014,
-    "has_ad": 0
+obj.vip.memberid = 20,
+obj.vip.expire_time = 1739785014,
+obj.vip.has_ad = 0
 
 $done({body: JSON.stringify(obj)});
