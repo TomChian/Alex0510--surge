860add145c70ad13e80032b8ca950ed5af5fec74
Eric <Alex0510@users.noreply.github.com>
2020-02-17 18:08:12 +0800

    Update wps1.js

--- Script/wps1.js
+++ Script/wps1.js
@@ -1,7 +1,21 @@
 let obj = JSON.parse($response.body);
 
-obj.vip.memberid = 20,
-obj.vip.expire_time = 1739785014,
-obj.vip.has_ad = 0
+obj= {
+  "result": "ok",
+  "total_cost": 0,
+  "privilege": [
+  ],
+  "userid": 123014047,
+  "wealth": 0,
+  "level": 5,
+  "exp": 0,
+  "vip": {
+    "memberid": 20,
+    "expire_time": 1739785014,
+    "name": "Register",
+    "has_ad": 0
+  },
+  "total_buy": 1
+}
 
 $done({body: JSON.stringify(obj)});
