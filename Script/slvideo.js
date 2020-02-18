db18ec45fb81c238e280eae2a7bd354e0118043a
Eric <Alex0510@users.noreply.github.com>
2020-02-19 00:43:00 +0800

    Update slvideo.js

--- Script/slvideo.js
+++ Script/slvideo.js
@@ -1,6 +1,7 @@
 var obj = JSON.parse($response.body);
 
-
+  obj.data["errorCode"] = 0,
+  obj.data["sandbox"] = 0,
   obj.data["expireTime"] = 1866117937,
   obj.data["productId"] = SaladVip_Int01,
   obj.data["appleVip"] = 1,
