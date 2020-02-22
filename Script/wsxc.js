eb411f672095f22ea18336001b776d11edd0b2c7
Eric <Alex0510@users.noreply.github.com>
2020-02-22 15:01:53 +0800

    Update wsxc.js

--- Script/wsxc.js
+++ Script/wsxc.js
@@ -1,14 +1,13 @@
 /*
-微商相册正则
+微商相册自慰
 https:\/\/www\.wsxcme\.com\/service\/account\/user_info_operation
 
 hostname：*.wsxcme.com
 let obj = JSON.parse($response.body);
 
-obj. result = {
-    "is_vip": true,
-    "is_expire": false,
-    "deadline": "2029-02-13",
-}
+    obj. result.is_vip = true,
+    obj. result.is_expire = false,
+    obj. result.deadline = "2029-02-13",
+   
 
 $done({body: JSON.stringify(obj)});
