20591d1a3b22f2af4657151f5a8e609c3fff42ea
Eric <Alex0510@users.noreply.github.com>
2020-02-27 21:26:48 +0800

    美区

--- Script/wps1.js
+++ Script/wps1.js
@@ -1,21 +1,10 @@
 let obj = JSON.parse($response.body);
 
-obj= {
-  "result": "ok",
-  "total_cost": 0,
-  "privilege": [
-  ],
-  "userid": 123014047,
-  "wealth": 0,
-  "level": 5,
-  "exp": 0,
-  "vip": {
-    "memberid": 20,
-    "expire_time": 1739785014,
-    "name": "Register",
-    "has_ad": 0
-  },
-  "total_buy": 1
-}
+  obj.level = 5,
+  obj.exp = 0,
+  obj.vip.memberid = 20,
+  obj.vip.expire_time = 1739785014,
+  obj.vip.has_ad = 0,
+  obj.total_buy = 1
 
 $done({body: JSON.stringify(obj)});
