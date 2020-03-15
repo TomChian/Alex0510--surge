ae3800dcbb1dc9b3a1b867d6e8e5546f9ea6e304
Eric <Alex0510@users.noreply.github.com>
2020-03-15 14:15:25 +0800

    卡点视频

--- Script/kdsp.js
+++ Script/kdsp.js
@@ -7,6 +7,6 @@ let obj = JSON.parse($response.body);
 
 obj.data.isVip = 1,
 obj.data.type = 1,
-obj.data.expirationTime = 2099-12-14到期
+obj.data.expirationTime = "2099-12-14到期"
 
 $done({body: JSON.stringify(obj)});
