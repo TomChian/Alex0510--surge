3ebe8721deabbbc3770b60f51e173715a42be649
Eric <Alex0510@users.noreply.github.com>
2020-03-28 12:38:36 +0800

    内购恢复

--- Script/ng.js
+++ Script/ng.js
@@ -7,6 +7,7 @@ magi+ inshot peachy 发型多多内购恢复
 新增inscopy和instake永久会员
 新增Bear笔记、Manly修图、Picsew all in one
 新增DJ it如果无法恢复请先尝试免费试用在卸载重新恢复
+新增filmr视频编辑内购
 https://buy.itunes.apple.com/verifyReceipt
 hostname:buy.itunes.apple.com
 */
@@ -921,5 +922,80 @@ obj = {
   }]
 }
 }
-
+if(bundle_id == "com.stey")
+{
+obj = {
+  "status": 0,
+  "environment": "Production",
+  "receipt": {
+    "receipt_type": "Production",
+    "app_item_id": 1171358257,
+    "receipt_creation_date": "2020-03-28 03:32:47 Etc/GMT",
+    "bundle_id": "com.stey",
+    "original_purchase_date": "2020-03-28 03:09:30 Etc/GMT",
+    "in_app": [
+      {
+        "quantity": "1",
+        "purchase_date_ms": "1585366105000",
+        "expires_date": "2099-04-04 03:28:25 Etc/GMT",
+        "expires_date_pst": "2099-04-03 20:28:25 America/Los_Angeles",
+        "is_in_intro_offer_period": "false",
+        "transaction_id": "160000708109552",
+        "is_trial_period": "true",
+        "original_transaction_id": "160000708109552",
+        "purchase_date": "2020-03-28 03:28:25 Etc/GMT",
+        "product_id": "com.filmr.year",
+        "original_purchase_date_pst": "2020-03-27 20:28:26 America/Los_Angeles",
+        "original_purchase_date_ms": "1585366106000",
+        "web_order_line_item_id": "160000253318339",
+        "expires_date_ms": "4078957154000",
+        "purchase_date_pst": "2020-03-27 20:28:25 America/Los_Angeles",
+        "original_purchase_date": "2020-03-28 03:28:26 Etc/GMT"
+      }
+    ],
+    "adam_id": 1171358257,
+    "receipt_creation_date_pst": "2020-03-27 20:32:47 America/Los_Angeles",
+    "request_date": "2020-03-28 03:33:33 Etc/GMT",
+    "request_date_pst": "2020-03-27 20:33:33 America/Los_Angeles",
+    "version_external_identifier": 835168703,
+    "request_date_ms": "1585366413063",
+    "original_purchase_date_pst": "2020-03-27 20:09:30 America/Los_Angeles",
+    "application_version": "8",
+    "original_purchase_date_ms": "1585364970000",
+    "receipt_creation_date_ms": "1585366367000",
+    "original_application_version": "8",
+    "download_id": 36067885010339
+  },
+  "pending_renewal_info": [
+    {
+      "product_id": "com.filmr.year",
+      "original_transaction_id": "160000708109552",
+      "auto_renew_product_id": "com.filmr.year",
+      "auto_renew_status": "0"
+    }
+  ],
+  "latest_receipt_info": [
+    {
+      "quantity": "1",
+      "purchase_date_ms": "1585366105000",
+      "expires_date": "2099-04-04 03:28:25 Etc/GMT",
+      "expires_date_pst": "2099-04-03 20:28:25 America/Los_Angeles",
+      "is_in_intro_offer_period": "false",
+      "transaction_id": "160000708109552",
+      "is_trial_period": "true",
+      "original_transaction_id": "160000708109552",
+      "purchase_date": "2020-03-28 03:28:25 Etc/GMT",
+      "product_id": "com.filmr.year",
+      "original_purchase_date_pst": "2020-03-27 20:28:26 America/Los_Angeles",
+      "subscription_group_identifier": "20443053",
+      "original_purchase_date_ms": "1585366106000",
+      "web_order_line_item_id": "160000253318339",
+      "expires_date_ms": "4078957154000",
+      "purchase_date_pst": "2020-03-27 20:28:25 America/Los_Angeles",
+      "original_purchase_date": "2020-03-28 03:28:26 Etc/GMT"
+    }
+  ],
+  "latest_receipt": "MIIUAAYJKoZIhvcNAQcCoIIT8TCCE+0CAQExCzAJBgUrDgMCGgUAMIIDoQYJKoZIhvcNAQcBoIIDkgSCA44xggOKMAoCARQCAQEEAgwAMAsCAQMCAQEEAwwBODALAgETAgEBBAMMATgwCwIBGQIBAQQDAgEDMAwCAQoCAQEEBBYCNCswDAIBDgIBAQQEAgIAvTANAgENAgEBBAUCAwH8/DAOAgEBAgEBBAYCBEXRgjEwDgIBCQIBAQQGAgRQMjUzMA4CAQsCAQEEBgIEBcwNfDAOAgEQAgEBBAYCBDHHqb8wEAIBDwIBAQQIAgYgzbV5TaMwEgIBAgIBAQQKDAhjb20uc3RleTAUAgEAAgEBBAwMClByb2R1Y3Rpb24wGAIBBAIBAgQQ8yNC7T4zUrFSRpz73Z/HQjAcAgEFAgEBBBRN6tXr1aF9cefuRvfsop9HWhGvFzAeAgEIAgEBBBYWFDIwMjAtMDMtMjhUMDM6MzI6NDdaMB4CAQwCAQEEFhYUMjAyMC0wMy0yOFQwMzozMzozM1owHgIBEgIBAQQWFhQyMDIwLTAzLTI4VDAzOjA5OjMwWjA/AgEHAgEBBDdQv3INq723V6c+AW8R/Dh0HBhLygwcR2SxuNzoLXlHMZh5CqpRYMkaR1lzLI13bEduYhT5M1KWMFUCAQYCAQEETZHo9ACCk4ETXs7nn+76Xe5Eh5RHDPT7QmxAubbJ6jSFkpv3CeVp8o36GKfk10NddwNUaxdGK+V0x0ELfmsYn6Bo44LhWNu9ENKaOv8eMIIBfAIBEQIBAQSCAXIxggFuMAsCAgatAgEBBAIMADALAgIGsAIBAQQCFgAwCwICBrICAQEEAgwAMAsCAgazAgEBBAIMADALAgIGtAIBAQQCDAAwCwICBrUCAQEEAgwAMAsCAga2AgEBBAIMADAMAgIGpQIBAQQDAgEBMAwCAgarAgEBBAMCAQMwDAICBrECAQEEAwIBATAMAgIGtwIBAQQDAgEAMA8CAgauAgEBBAYCBFHezm0wEgICBq8CAQEECQIHAJGE9kNUwzAZAgIGpgIBAQQQDA5jb20uZmlsbXIueWVhcjAaAgIGpwIBAQQRDA8xNjAwMDA3MDgxMDk1NTIwGgICBqkCAQEEEQwPMTYwMDAwNzA4MTA5NTUyMB8CAgaoAgEBBBYWFDIwMjAtMDMtMjhUMDM6Mjg6MjVaMB8CAgaqAgEBBBYWFDIwMjAtMDMtMjhUMDM6Mjg6MjZaMB8CAgasAgEBBBYWFDIwMjAtMDQtMDRUMDM6Mjg6MjVaoIIOZTCCBXwwggRkoAMCAQICCA7rV4fnngmNMA0GCSqGSIb3DQEBBQUAMIGWMQswCQYDVQQGEwJVUzETMBEGA1UECgwKQXBwbGUgSW5jLjEsMCoGA1UECwwjQXBwbGUgV29ybGR3aWRlIERldmVsb3BlciBSZWxhdGlvbnMxRDBCBgNVBAMMO0FwcGxlIFdvcmxkd2lkZSBEZXZlbG9wZXIgUmVsYXRpb25zIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MB4XDTE1MTExMzAyMTUwOVoXDTIzMDIwNzIxNDg0N1owgYkxNzA1BgNVBAMMLk1hYyBBcHAgU3RvcmUgYW5kIGlUdW5lcyBTdG9yZSBSZWNlaXB0IFNpZ25pbmcxLDAqBgNVBAsMI0FwcGxlIFdvcmxkd2lkZSBEZXZlbG9wZXIgUmVsYXRpb25zMRMwEQYDVQQKDApBcHBsZSBJbmMuMQswCQYDVQQGEwJVUzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKXPgf0looFb1oftI9ozHI7iI8ClxCbLPcaf7EoNVYb/pALXl8o5VG19f7JUGJ3ELFJxjmR7gs6JuknWCOW0iHHPP1tGLsbEHbgDqViiBD4heNXbt9COEo2DTFsqaDeTwvK9HsTSoQxKWFKrEuPt3R+YFZA1LcLMEsqNSIH3WHhUa+iMMTYfSgYMR1TzN5C4spKJfV+khUrhwJzguqS7gpdj9CuTwf0+b8rB9Typj1IawCUKdg7e/pn+/8Jr9VterHNRSQhWicxDkMyOgQLQoJe2XLGhaWmHkBBoJiY5uB0Qc7AKXcVz0N92O9gt2Yge4+wHz+KO0NP6JlWB7+IDSSMCAwEAAaOCAdcwggHTMD8GCCsGAQUFBwEBBDMwMTAvBggrBgEFBQcwAYYjaHR0cDovL29jc3AuYXBwbGUuY29tL29jc3AwMy13d2RyMDQwHQYDVR0OBBYEFJGknPzEdrefoIr0TfWPNl3tKwSFMAwGA1UdEwEB/wQCMAAwHwYDVR0jBBgwFoAUiCcXCam2GGCL7Ou69kdZxVJUo7cwggEeBgNVHSAEggEVMIIBETCCAQ0GCiqGSIb3Y2QFBgEwgf4wgcMGCCsGAQUFBwICMIG2DIGzUmVsaWFuY2Ugb24gdGhpcyBjZXJ0aWZpY2F0ZSBieSBhbnkgcGFydHkgYXNzdW1lcyBhY2NlcHRhbmNlIG9mIHRoZSB0aGVuIGFwcGxpY2FibGUgc3RhbmRhcmQgdGVybXMgYW5kIGNvbmRpdGlvbnMgb2YgdXNlLCBjZXJ0aWZpY2F0ZSBwb2xpY3kgYW5kIGNlcnRpZmljYXRpb24gcHJhY3RpY2Ugc3RhdGVtZW50cy4wNgYIKwYBBQUHAgEWKmh0dHA6Ly93d3cuYXBwbGUuY29tL2NlcnRpZmljYXRlYXV0aG9yaXR5LzAOBgNVHQ8BAf8EBAMCB4AwEAYKKoZIhvdjZAYLAQQCBQAwDQYJKoZIhvcNAQEFBQADggEBAA2mG9MuPeNbKwduQpZs0+iMQzCCX+Bc0Y2+vQ+9GvwlktuMhcOAWd/j4tcuBRSsDdu2uP78NS58y60Xa45/H+R3ubFnlbQTXqYZhnb4WiCV52OMD3P86O3GH66Z+GVIXKDgKDrAEDctuaAEOR9zucgF/fLefxoqKm4rAfygIFzZ630npjP49ZjgvkTbsUxn/G4KT8niBqjSl/OnjmtRolqEdWXRFgRi48Ff9Qipz2jZkgDJwYyz+I0AZLpYYMB8r491ymm5WyrWHWhumEL1TKc3GZvMOxx6GUPzo22/SGAGDDaSK+zeGLUR2i0j0I78oGmcFxuegHs5R0UwYS/HE6gwggQiMIIDCqADAgECAggB3rzEOW2gEDANBgkqhkiG9w0BAQUFADBiMQswCQYDVQQGEwJVUzETMBEGA1UEChMKQXBwbGUgSW5jLjEmMCQGA1UECxMdQXBwbGUgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkxFjAUBgNVBAMTDUFwcGxlIFJvb3QgQ0EwHhcNMTMwMjA3MjE0ODQ3WhcNMjMwMjA3MjE0ODQ3WjCBljELMAkGA1UEBhMCVVMxEzARBgNVBAoMCkFwcGxlIEluYy4xLDAqBgNVBAsMI0FwcGxlIFdvcmxkd2lkZSBEZXZlbG9wZXIgUmVsYXRpb25zMUQwQgYDVQQDDDtBcHBsZSBXb3JsZHdpZGUgRGV2ZWxvcGVyIFJlbGF0aW9ucyBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMo4VKbLVqrIJDlI6Yzu7F+4fyaRvDRTes58Y4Bhd2RepQcjtjn+UC0VVlhwLX7EbsFKhT4v8N6EGqFXya97GP9q+hUSSRUIGayq2yoy7ZZjaFIVPYyK7L9rGJXgA6wBfZcFZ84OhZU3au0Jtq5nzVFkn8Zc0bxXbmc1gHY2pIeBbjiP2CsVTnsl2Fq/ToPBjdKT1RpxtWCcnTNOVfkSWAyGuBYNweV3RY1QSLorLeSUheHoxJ3GaKWwo/xnfnC6AllLd0KRObn1zeFM78A7SIym5SFd/Wpqu6cWNWDS5q3zRinJ6MOL6XnAamFnFbLw/eVovGJfbs+Z3e8bY/6SZasCAwEAAaOBpjCBozAdBgNVHQ4EFgQUiCcXCam2GGCL7Ou69kdZxVJUo7cwDwYDVR0TAQH/BAUwAwEB/zAfBgNVHSMEGDAWgBQr0GlHlHYJ/vRrjS5ApvdHTX8IXjAuBgNVHR8EJzAlMCOgIaAfhh1odHRwOi8vY3JsLmFwcGxlLmNvbS9yb290LmNybDAOBgNVHQ8BAf8EBAMCAYYwEAYKKoZIhvdjZAYCAQQCBQAwDQYJKoZIhvcNAQEFBQADggEBAE/P71m+LPWybC+P7hOHMugFNahui33JaQy52Re8dyzUZ+L9mm06WVzfgwG9sq4qYXKxr83DRTCPo4MNzh1HtPGTiqN0m6TDmHKHOz6vRQuSVLkyu5AYU2sKThC22R1QbCGAColOV4xrWzw9pv3e9w0jHQtKJoc/upGSTKQZEhltV/V6WId7aIrkhoxK6+JJFKql3VUAqa67SzCu4aCxvCmA5gl35b40ogHKf9ziCuY7uLvsumKV8wVjQYLNDzsdTJWk26v5yZXpT+RN5yaZgem8+bQp0gF6ZuEujPYhisX4eOGBrr/TkJ2prfOv/TgalmcwHFGlXOxxioK0bA8MFR8wggS7MIIDo6ADAgECAgECMA0GCSqGSIb3DQEBBQUAMGIxCzAJBgNVBAYTAlVTMRMwEQYDVQQKEwpBcHBsZSBJbmMuMSYwJAYDVQQLEx1BcHBsZSBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTEWMBQGA1UEAxMNQXBwbGUgUm9vdCBDQTAeFw0wNjA0MjUyMTQwMzZaFw0zNTAyMDkyMTQwMzZaMGIxCzAJBgNVBAYTAlVTMRMwEQYDVQQKEwpBcHBsZSBJbmMuMSYwJAYDVQQLEx1BcHBsZSBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTEWMBQGA1UEAxMNQXBwbGUgUm9vdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAOSRqQkfkdseR1DrBe1eeYQt6zaiV0xV7IsZid75S2z1B6siMALoGD74UAnTf0GomPnRymacJGsR0KO75Bsqwx+VnnoMpEeLW9QWNzPLxA9NzhRp0ckZcvVdDtV/X5vyJQO6VY9NXQ3xZDUjFUsVWR2zlPf2nJ7PULrBWFBnjwi0IPfLrCwgb3C2PwEwjLdDzw+dPfMrSSgayP7OtbkO2V4c1ss9tTqt9A8OAJILsSEWLnTVPA3bYharo3GSR1NVwa8vQbP4++NwzeajTEV+H0xrUJZBicR0YgsQg0GHM4qBsTBY7FoEMoxos48d3mVz/2deZbxJ2HafMxRloXeUyS0CAwEAAaOCAXowggF2MA4GA1UdDwEB/wQEAwIBBjAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBQr0GlHlHYJ/vRrjS5ApvdHTX8IXjAfBgNVHSMEGDAWgBQr0GlHlHYJ/vRrjS5ApvdHTX8IXjCCAREGA1UdIASCAQgwggEEMIIBAAYJKoZIhvdjZAUBMIHyMCoGCCsGAQUFBwIBFh5odHRwczovL3d3dy5hcHBsZS5jb20vYXBwbGVjYS8wgcMGCCsGAQUFBwICMIG2GoGzUmVsaWFuY2Ugb24gdGhpcyBjZXJ0aWZpY2F0ZSBieSBhbnkgcGFydHkgYXNzdW1lcyBhY2NlcHRhbmNlIG9mIHRoZSB0aGVuIGFwcGxpY2FibGUgc3RhbmRhcmQgdGVybXMgYW5kIGNvbmRpdGlvbnMgb2YgdXNlLCBjZXJ0aWZpY2F0ZSBwb2xpY3kgYW5kIGNlcnRpZmljYXRpb24gcHJhY3RpY2Ugc3RhdGVtZW50cy4wDQYJKoZIhvcNAQEFBQADggEBAFw2mUwteLftjJvc83eb8nbSdzBPwR+Fg4UbmT1HN/Kpm0COLNSxkBLYvvRzm+7SZA/LeU802KI++Xj/a8gH7H05g4tTINM4xLG/mk8Ka/8r/FmnBQl8F0BWER5007eLIztHo9VvJOLr0bdw3w9F4SfK8W147ee1Fxeo3H4iNcol1dkP1mvUoiQjEfehrI9zgWDGG1sJL5Ky+ERI8GA4nhX1PSZnIIozavcNgs/e66Mv+VNqW2TAYzN39zoHLFbr2g8hDtq6cxlPtdk2f8GHVdmnmbkyQvvY1XGefqFStxu9k0IkEirHDx22TZxeY8hLgBdQqorV2uT80AkHN7B1dSExggHLMIIBxwIBATCBozCBljELMAkGA1UEBhMCVVMxEzARBgNVBAoMCkFwcGxlIEluYy4xLDAqBgNVBAsMI0FwcGxlIFdvcmxkd2lkZSBEZXZlbG9wZXIgUmVsYXRpb25zMUQwQgYDVQQDDDtBcHBsZSBXb3JsZHdpZGUgRGV2ZWxvcGVyIFJlbGF0aW9ucyBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eQIIDutXh+eeCY0wCQYFKw4DAhoFADANBgkqhkiG9w0BAQEFAASCAQBlXCkEHv84AJvK3UipeRe/r/46FqtvvU2xIKBc9wMGcEzZ6aQqkpaq7OvRrCCI0qhK1ICkQMiE8hWzNlLKFF10Zp+pGedBbwQP/8D+tcqxYsTfKFEIbWco7umWY3mHS/+K73dY9Exya8zaHIoxqItmPL9jdRrlQMLu9lIbNlkgMEUn47jStMfIwKqdA4JOJRGSqsQbLZ6/UuWO2Q4veEnKpK0w1xyvvk3A+T72cKAtvMyZliwS5NC1HOeXmSa9+UlmykinC3PzXfGHbIvW7WXK1bQi1+Y0Acxa+Z9cfmfIr9YYsjj6Xj2gpPpiNa44+HVaNMeIYsUBoZMsaB2GMLEK"
+}
+}
 $done({body: JSON.stringify(obj)});
