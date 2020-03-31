eca8191500a098cc2737a38b497223e128fe6275
Eric <Alex0510@users.noreply.github.com>
2020-03-31 12:49:35 +0800

    熊猫助手黄金版

--- Script/panda.js
+++ Script/panda.js
@@ -1,23 +1,15 @@
 /*
 熊猫助手黄金版下载地址https://www.lanzous.com/iatt06j
 如果无法安装自行下载IPA重新签名安装即可
-去除app显示到期限制
+去除app显示到期限制--Eric
 */
 
 
 let obj = JSON.parse($response.body);
 
-obj.Data = {
-    "InvalidDays": 999,
-    "InvalidTime": "2099-01-25",
-    "Status": 1,
-    "Udid": "fdc2037e2041684081fe1bc42eb52d2aa8816a58",
-    "SubscribeVip": 1,
-    "ActualTotal": 0,
-    "ClientShow": 0,
-    "Uid": 0,
-    "NextTime": null,
-    "CreateTime": "2018-01-29"
-  }
-}
+obj.Data.InvalidDays = 999,
+obj.Data.InvalidTime = "2099-01-25",
+obj.Data.Status = 1,
+obj.Data.SubscribeVip = 1
+
 $done({body: JSON.stringify(obj)});
